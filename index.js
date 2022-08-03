ea568767051da2a2d89e4bfc85210e6051b9247c
codalo <ckodalo@gmail.com>
2022-08-04 02:14:14 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    let result = a + b;
+    return result;
+}
+
+function subtract(a, b) {
+    let result = a - b;
+    return result;
+}
+
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+function divide(a, b) {
+    let result = a/b;
+    return result;
+}
+function makeInt(string) {
+   let result = parseInt(string, 10);
+    return result;
+}
+function preserveDecimal(string) {
+    let result = parseFloat(string);
+    return result;
+}
+function increment(n){
+    n += 1;
+    return n;
+}
+function decrement (n) {
+    n -= 1;
+    return n;
+}
